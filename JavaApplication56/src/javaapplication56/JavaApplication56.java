050481b0bb44c2bfd8943f8188985b75ae5fe0fa
carmesto91 <38926661+Mrj11@users.noreply.github.com>
2018-05-02 22:01:50 -0300

    primera subida

---
+++ JavaApplication56/src/javaapplication56/JavaApplication56.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication56;
+
+/**
+ *
+ * @author alumno
+ */
+//import java.util.Scanner;
+public class JavaApplication56 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[]numeros;
+        numeros=new int[25];
+        
+        for(int i=0;i<25;i++){
+            numeros[i]=11+11*i;
+            System.out.println(numeros[i]);
+        }
+          
+}
+}   
+
+    
+    
+
